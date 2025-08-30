791922947a47c9ecc8ca2dbac776d67920f1c798
kowshik76 <kowshiksangada@gmail.com>
2025-08-30 22:32:53 +0530

    Added auth and Hero section wtih Navbar and Cards with integrating puter.js

---
+++ app/routes/auth.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { usePuterStore } from "~/lib/puter";
+export const meta = () => [
+  { title: "SkillSync || Account" },
+  { name: "description", content: "Log into your account" },
+];
+const auth = () => {
+  const { isLoading, auth } = usePuterStore();
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+      <div className="gradient-border shadow-lg">
+        <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+          <div className="flex flex-col items-center gap-2 text-center">
+            <h1>welcome</h1>
+            <h2>Login to Continue</h2>
+          </div>
+          <div>
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                <p>Signing you..</p>
+              </button>
+            ) : (
+              <>
+                {auth.isAuthenticated ? (
+                  <button className="auth-button" onClick={auth.signOut}>
+                    <p>Log Out</p>
+                  </button>
+                ) : (
+                  <button className="auth-button" onClick={auth.signIn}>
+                    <p> Sign In</p>
+                  </button>
+                )}
+              </>
+            )}
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default auth;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "SkillSync" },
+    {
+      name: "description",
+      content:
+        "SkillSync is an AI-powered resume analyzer that helps job seekers optimize their resumes with real-time insights. Get instant feedback on skills, formatting, ATS compatibility, and keyword optimization to improve your chances of landing interviews.",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track your Resume Ratings and Scores with AI-powered insights</h1>
+          <h2>
+            AI-powered scores and insights to highlight strengths, fix gaps, and
+            optimize your resume.
+          </h2>
+        </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
